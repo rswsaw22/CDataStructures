b3fc984b3e3219071c9f3d0b33c8099e46f1bc2a
rswsaw22 <week0316@gmail.com>
2020-12-22 13:14:34 -0700

    New file structure for vector

---
+++ vector/vector.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct vec_t {
+    void *_array;
+    int size;
+    int maxIndex;
+};
+
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
+}
